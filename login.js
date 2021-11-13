1435278d2ec307caec783305a07005ca4c43bda7
Andres Velazquez <39769112+andresvelazq@users.noreply.github.com>
2021-11-13 17:12:04 -0500

    login button
    placeholder

---
+++ login.js
@@ -0,0 +1,7 @@
+// import React, { useState } from "react";
+// import { useNavigate } from "./src/addulting/node_modules/react-router";
+// import { signInWithGoogle } from "./src/addulting/src/firebase";
+
+function Login() {
+    
+}
--- src/addulting/src/App.js
+++ src/addulting/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Dashboard from './Dashboard';
 import Login from './Login';
 //import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-          <Login/>
+        <Login/>
+        <Dashboard/>
     </div>
   );
 }
