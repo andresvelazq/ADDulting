ea2ee944c24199f0f4a1733aebe36bee28728315
Andres Velazquez <39769112+andresvelazq@users.noreply.github.com>
2021-11-13 22:59:44 -0500

    add tasks, pretty login
    added functionality to add tasks and delete the new tasks
    edited login screen to not be ugly and white

--- login.js
+++ login.js
@@ -1,34 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+//import { initializeApp } from "firebase/app";
+//import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 //import { getAuth, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 //import { getFirestore } from "firebase/firestore"; 
-
-const app = initializeApp({
-    apiKey: "AIzaSyBpdAmST1s6FD9yAmCrUoPLQ-mg8BGXCac",
-    authDomain: "addulting-fc16e.firebaseapp.com",
-    projectId: "addulting-fc16e",
-    storageBucket: "addulting-fc16e.appspot.com",
-    messagingSenderId: "941943380813",
-    appId: "1:941943380813:web:dcf31919ed4cdc5c493504",
-    measurementId: "G-89VD8JX662"
-  });
-
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
-
-const signInWithGoogle = async () => {
-    signInWithPopup(auth, googleProvider)
-        .then((result) => {
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            const user=result;
-        }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            const email = error.email;
-            const credential = GoogleAuthProvider.credentialFromError(error);
-        })
-};
+import { signInWithGoogle } from "./firebase";
 
 function googleLogin() {
     signInWithGoogle;
